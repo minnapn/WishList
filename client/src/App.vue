53cbf27f0214cf8ab30f0d9ff367c9407ca4d936
Minna Pettersson <minnapn@hotmail.com>
2020-07-24 11:49:55 +0200

    Started creating client with Vue

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Wishes />
+  </div>
+</template>
+
+<script>
+  import Wishes from './components/Wishes'
+  const app = {
+    name: 'app',
+    components: {
+      Wishes
+    },
+    data: () => {
+      return {
+
+      }
+    }
+  }
+
+  export default app
+</script>
+
+<style>
+  [v-cloak] {display: none;}
+</style>
---
+++ client/src/components/Wishes.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="title">Wishes</h1>
+        <section class="wishlistapp">
+            <div v-if="loading">
+                <h1 class = "loading">Loading...</h1>
+            </div>
+            <div v-else>
+                <header class="header">
+                    <input class="new-wish"
+                        autofocus autocomplete="off"
+                        :placeholder="this.inputPlaceholder"
+                        v-model="newWish"
+                        @keyup.enter="addWish">
+                </header>                
+            </div>
+        </section>        
+    </div>
+</template>
+
